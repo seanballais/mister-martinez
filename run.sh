60787d74f64bfa1ae05f5131ca659206f8ba7bc2
Sean Francis N. Ballais <sfballais123@gmail.com>
2016-04-18 08:20:05 +0800

    Using the Cleverbot API now.

--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Run manager thing
-if [ ! -f bin/senbuild ]; then
-    echo "Senbuild not built yet. Building senbuild..."
-    mkdir -p bin/
-    clang -o bin/senbuild senbuild/src/main.c senbuild/src/senbuild_funcs.c
-    echo "Finished building Senbuild."
-    echo "Make sure to create a text file for senbuild to base a sentence from."
-fi
-
 # Make Mister Martinez talk
 node server.js
 echo "Done."
